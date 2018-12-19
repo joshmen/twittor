7cab4de74d46b321c6495d9c4dc155359b64c11c
joshmen <xjoshmenx@gmail.com>
2018-12-19 00:56:04 -0700

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -7,9 +7,8 @@ const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+    //'/',
     'index.html',
-    
     'css/style.css',
     'img/favicon.ico',
     'img/avatars/spiderman.jpg',
